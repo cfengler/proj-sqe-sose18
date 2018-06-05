e5b37fb1043493494fa579ca8fa2ff48a2de4be5
CFengler <christian.fengler@yahoo.de>
2018-06-05 16:41:43 +0200

    TestDataProjectRepository

---
+++ src/main/java/de/tuberlin/sqe/ss18/reqexchange/testdata/common/repository/TestDataProjectRepository.java
@@ -0,0 +1,7 @@
+package de.tuberlin.sqe.ss18.reqexchange.testdata.common.repository;
+
+import de.tuberlin.sqe.ss18.reqexchange.data.common.repository.ProjectRepository;
+
+public class TestDataProjectRepository implements ProjectRepository {
+    //TODO: hier soll ohne Datenquelle ein paar Daten zurückgegeben werden
+}
