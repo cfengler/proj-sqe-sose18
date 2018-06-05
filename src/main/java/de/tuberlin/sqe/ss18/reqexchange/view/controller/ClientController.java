d0b8b487c1bb557f6e7a471825034a2eacefc7ad
JiMMyTry <mkbrieler@icloud.com>
2018-06-05 23:45:55 +0200

    first changes to implement new UI

--- src/main/java/de/tuberlin/sqe/ss18/reqexchange/view/controller/ClientController.java
+++ src/main/java/de/tuberlin/sqe/ss18/reqexchange/view/controller/ClientController.java
@@ -1,12 +1,16 @@
 package de.tuberlin.sqe.ss18.reqexchange.view.controller;
 
 import de.tuberlin.sqe.ss18.reqexchange.view.viewmodel.ClientViewModel;
+import de.tuberlin.sqe.ss18.reqexchange.view.viewmodel.ProjectViewModel;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,12 +18,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClientController {
 
-    @FXML private VBox vBoxMenu;
-    @FXML private Button buttonFunction1;
-    @FXML private Button buttonFunction2;
-    @FXML private Button buttonFunction3;
-
-    @FXML private FlowPane flowPaneProjects;
+    @FXML private HBox hBoxActions;
+    @FXML private Button buttonCreateProject;
+    @FXML private Button buttonJoinProject;
+    @FXML private TilePane tilePaneProjects;
 
     private ClientViewModel clientViewModel;
 
@@ -29,19 +31,14 @@ public class ClientController {
         System.out.println(getClass().getSimpleName() + " ctor");
 
         this.clientViewModel = clientViewModel;
+
+        ListView<ProjectViewModel> test = new ListView<>();
+        TilePane tiles = new TilePane();
     }
 
     @FXML
     public void initialize() {
-        initializeEventHandlers();
-
         initializeFlowPaneProjects();
-    }
-
-    private void initializeEventHandlers() {
-        buttonFunction1.setOnAction(buttonFunction1_onAction);
-        buttonFunction2.setOnAction(buttonFunction1_onAction);
-        buttonFunction3.setOnAction(buttonFunction1_onAction);
     }
 
     private EventHandler<ActionEvent> buttonFunction1_onAction = e -> {
@@ -51,12 +48,20 @@ public class ClientController {
         alert.showAndWait();
     };
 
+    @FXML protected void handleButtonCreateProjectAction(ActionEvent event) {
+        //TODO handle create project button action
+    }
+
+    @FXML protected void handleButtonJoinProjectAction(ActionEvent event) {
+        //TODO handle join project button action
+    }
+
     private void initializeFlowPaneProjects() {
         for (int i = 0; i < 15; i++) {
             Button newButton = new Button();
             newButton.setMinWidth(200);
             newButton.setMinHeight(200);
-            flowPaneProjects.getChildren().add(newButton);
+            tilePaneProjects.getChildren().add(newButton);
         }
     }
 }
---
+++ src/main/java/de/tuberlin/sqe/ss18/reqexchange/view/viewmodel/ProjectViewModel.java
@@ -0,0 +1,71 @@
+package de.tuberlin.sqe.ss18.reqexchange.view.viewmodel;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class ProjectViewModel {
+
+    private SimpleStringProperty name;
+    private SimpleBooleanProperty localChanged;
+    private SimpleBooleanProperty serverChanged;
+
+    public ProjectViewModel() {
+        this(null, false, false);
+    }
+
+    public ProjectViewModel(String name) {
+        this(name, false, false);
+    }
+
+    public ProjectViewModel(String name, boolean localChanged, boolean serverChanged) {
+        this.name = new SimpleStringProperty(name);
+        this.localChanged = new SimpleBooleanProperty(localChanged);
+        this.serverChanged = new SimpleBooleanProperty(serverChanged);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public boolean isLocalChanged() {
+        return localChanged.get();
+    }
+
+    public SimpleBooleanProperty localChangedProperty() {
+        return localChanged;
+    }
+
+    public void setLocalChanged(boolean localChanged) {
+        this.localChanged.set(localChanged);
+    }
+
+    public boolean isServerChanged() {
+        return serverChanged.get();
+    }
+
+    public SimpleBooleanProperty serverChangedProperty() {
+        return serverChanged;
+    }
+
+    public void setServerChanged(boolean serverChanged) {
+        this.serverChanged.set(serverChanged);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProjectViewModel that = (ProjectViewModel) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+}
