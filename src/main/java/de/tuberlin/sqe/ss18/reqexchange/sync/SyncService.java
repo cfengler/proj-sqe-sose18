39c48b9f8ad50418a6cc5d9e2b327b4e665887a2
CFengler <christian.fengler@yahoo.de>
2018-06-12 16:21:10 +0200

    remove server namespace reorder client namespace to application namespaces
    NOT DONE

---
+++ src/main/java/de/tuberlin/sqe/ss18/reqexchange/sync/SyncService.java
@@ -0,0 +1,12 @@
+package de.tuberlin.sqe.ss18.reqexchange.sync;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SyncService {
+
+    public void sync() {
+
+    }
+
+}
