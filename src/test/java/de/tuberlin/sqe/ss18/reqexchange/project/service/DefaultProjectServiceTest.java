6abf66b1bc95ee6f0ec535fec74ddc16c46670f0
CFengler <christian.fengler@yahoo.de>
2018-06-23 21:33:29 +0200

    prepare unit test

--- src/test/java/de/tuberlin/sqe/ss18/reqexchange/project/service/DefaultProjectServiceTest.java
+++ src/test/java/de/tuberlin/sqe/ss18/reqexchange/project/service/DefaultProjectServiceTest.java
@@ -1,4 +1,51 @@
 package de.tuberlin.sqe.ss18.reqexchange.project.service;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import de.tuberlin.sqe.ss18.reqexchange.ReqExchangeModule;
+import de.tuberlin.sqe.ss18.reqexchange.project.domain.Project;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DefaultProjectServiceTest {
+
+    private static Injector injector;
+    private static ProjectService projectService;
+
+    @BeforeClass
+    public static void startUp() {
+        injector = Guice.createInjector(new ReqExchangeModule());
+
+        projectService = injector.getInstance(ProjectService.class);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+    }
+
+    @Test
+    public void test_02a_CreateProject() {
+        //projectService.create("proj-sqe-sose18-unittest", ???, )
+
+        //projectService.create();
+    }
+
+    @Test
+    public void test_02b_JoinProject() {
+
+    }
+
+    @Test
+    public void test_02c_JoinProject() {
+
+    }
+
+
+
+
 }
